ddca5322a204893a7b2ba611f274ce5f353b989e
Curtis Lisle <clisle@knowledgevis.com>
2023-08-17 17:11:31 -0400

    generalized algorithm to work with multiple selection attributes

--- aql_simplify.py
+++ aql_simplify.py
@@ -1,15 +1,26 @@
 from arango import ArangoClient
-
+import time
 
 miserablesDatabase = "w-541c3500f9944ce395537e09a61b8b97"
 originalGraphName = 'miserables'
-reducedGraphName = 'reduced_'+originalGraphName
+method = 'SpeakerListener'
+#method = 'LabelProp'
+reducedGraphName = 'reduced_'+originalGraphName+'_'+method
 originalNodeCollectionName = 'characters'
 originalEdgeCollectionName = 'relationships'
-reducedNodeCollectionName = 'reducedNodes'
-reducedEdgeCollectionName = 'reducedEdges'
-
+reducedNodeCollectionName = 'reducedNodes_'+method
+reducedEdgeCollectionName = 'reducedEdges_'+method
 '''
+
+kg_db = 'w-70e8ac78da6041349894f13349faa7a1'
+originalGraphName = 'tiny'
+method = 'SpeakerListener'
+reducedGraphName = 'reduced_'+originalGraphName
+originalNodeCollectionName = 'kg_25_nodes'
+originalEdgeCollectionName = 'kg_20_edges'
+reducedNodeCollectionName = originalNodeCollectionName+'_reduced'
+reducedEdgeCollectionName = originalEdgeCollectionName + '_reduced'
+
 eurovisDatabase = 'w-f3bcbd142a0b405687cd82a068f26c39'
 originalGraphName = 'eurovis'
 reducedGraphName = 'reduced_'+originalGraphName
@@ -17,27 +28,79 @@ originalNodeCollectionName = 'people'
 originalEdgeCollectionName = 'connections'
 reducedNodeCollectionName = 'reducedNodes'
 reducedEdgeCollectionName = 'reducedEdges'
+
+
 '''
 
+def AddCommunityDetection(method,arango_db,graphName):
+    if method =='LabelProp':
+        print("running label propogation on:",graphName)
+        label_prop_job_id = arango_db.pregel.create_job(
+            graph=graphName,
+            algorithm='labelpropagation',
+            store=True,
+            async_mode=False,
+            result_field='_community_LP'
+            )
+        while ((arango_db.pregel.job(label_prop_job_id)['state'] == 'running') or
+            (arango_db.pregel.job(label_prop_job_id)['state'] == 'storing')):
+            time.sleep(0.25)
+            print('waiting for community label propogation job to finish')
 
-def CreateSimplifiedGraph(databaseName,originalGraphName,originalEdgeCollectionName,originalNodeCollectionName, reducedGraphName,reducedEdgeCollectionName,reducedNodeCollectionName,nameField="name"):
+    elif method == 'SpeakerListener':
+        print("running speaker-Listener label propogation on:",graphName)
+        slpa_job_id = arango_db.pregel.create_job(
+            graph=graphName,
+            algorithm='slpa',
+            store=True,
+            async_mode=False,
+            result_field='_community_SLPA'
+        )
+        while ((arango_db.pregel.job(slpa_job_id)['state'] == 'running') or 
+                (arango_db.pregel.job(slpa_job_id)['state'] == 'storing')):
+            time.sleep(0.25)
+            print('waiting for SLPA community job to finish')
+    print('community detection additions are finished')
+
+
+
+
+def CreateSimplifiedGraph(databaseName,originalGraphName,originalEdgeCollectionName,originalNodeCollectionName, reducedGraphName,reducedEdgeCollectionName,reducedNodeCollectionName,
+                          nameField="name",method='SpeakerListener',thresholdGuidance=0.8):
  
     # Initialize the client for ArangoDB.
     client = ArangoClient(hosts="http://localhost:8529")
     # Connect to "miserables" database as root user.
     db = client.db(databaseName, username="root", password="letmein")
 
+    # depending on he method used to reduce the graph, we need to check if the nodes
+    # already have the proper attributes.  We pull one node and look at its attributes.
+    cursor = db.aql.execute("FOR doc IN "+ originalNodeCollectionName + " LIMIT 1 RETURN doc")
+    sample_node = [document for document in cursor]
+    print('extracted ',len(sample_node),' sample from', originalNodeCollectionName+' in the '+ originalGraphName +' graph')
+
+    # now add attributes if they are needed and are not already present in the source graph
+    if method == "SpeakerListener" and '_community_SLPA' not in sample_node:
+        AddCommunityDetection(method,db,originalGraphName)
+    elif method == "LabelPropogation" and '_community_LP' not in sample_node:
+        AddCommunityDetection(method,db,originalGraphName)
+
+
     # create the output collections if they don't exist.  We will make a new regular
     # collection for the nodes and a new edge collection for the new edges.  The new
     # edges will connect between nodes of the new output node collection
 
-    if db.has_graph(reducedGraphName):
-        newGraph = db.graph(reducedGraphName)
-    else:
-        newGraph = db.create_graph(reducedGraphName)
+    if db.has_collection(reducedNodeCollectionName):
+        db.delete_collection(reducedNodeCollectionName)
+    newNodeCol = db.create_collection(reducedNodeCollectionName)
 
-    if not db.has_collection(reducedNodeCollectionName):
-        newNodeCol = db.create_collection(reducedNodeCollectionName)
+    if db.has_graph(reducedGraphName):
+        existingGraph = db.graph(reducedGraphName)
+        if existingGraph.has_edge_definition(reducedEdgeCollectionName):
+            existingGraph.delete_edge_definition(reducedEdgeCollectionName,purge=True)
+        db.delete_graph(reducedGraphName)
+    newGraph = db.create_graph(reducedGraphName)
+
     if not newGraph.has_edge_definition(reducedEdgeCollectionName):
         newEdgeCol = newGraph.create_edge_definition(
             edge_collection= reducedEdgeCollectionName,
@@ -45,14 +108,20 @@ def CreateSimplifiedGraph(databaseName,originalGraphName,originalEdgeCollectionN
             to_vertex_collections=[reducedNodeCollectionName]
         )
 
+
     # Execute an AQL query and iterate through the result cursor to find how many nodes are in the graph
-    cursor = db.aql.execute("FOR doc IN "+ originalNodeCollectionName + " RETURN doc")
-    node_names = [document[nameField] for document in cursor]
+    cursor = db.aql.execute("FOR doc IN "+ originalNodeCollectionName + " RETURN doc."+nameField)
+    node_names = [document for document in cursor]
     print('found ',len(node_names), originalNodeCollectionName+' in the '+ originalGraphName +' graph')
 
     # now we do the actual query we are looking for.  This returns a list of the nodes 
     # which touch a node that has a different community type than it is.  This is the subset
     # of all the nodes in the graph, which we want to have in our simplified graph
+
+    if method == 'SpeakerListener':
+        method_attribute = '_community_SLPA'
+    elif method == "LabelProp":
+        method_attribute = '_community_LP'
 
     query_str = 'RETURN UNIQUE(\
                     FOR n in '+originalNodeCollectionName+' \
@@ -60,10 +129,10 @@ def CreateSimplifiedGraph(databaseName,originalGraphName,originalEdgeCollectionN
                             in 1..1 \
                             any n \
                             graph ' + originalGraphName + ' \
-                            filter n._community_SLPA != k._community_SLPA \
+                            filter n.@selectionAttribute != k.@selectionAttribute \
                             return k) \
         '
-    bind_vars = {}
+    bind_vars = {'selectionAttribute': method_attribute}
     cursor = db.aql.execute(query=query_str, bind_vars=bind_vars)
     boundary_node_return = [doc for doc in cursor]
     boundary_node_list = boundary_node_return[0]
@@ -88,11 +157,11 @@ def CreateSimplifiedGraph(databaseName,originalGraphName,originalEdgeCollectionN
     # community.  All edges between same community nodes will be filtered out. This technique
     # will reduce the graph but will leave some nodes floating without an edge. 
 
-    bind_vars = {"nodeIds": nodeIds,"nodeDict": nodeDict}
+    bind_vars = {"nodeIds": nodeIds,"nodeDict": nodeDict,'selectionAttribute': method_attribute}
     query_str = 'FOR e in '+ originalEdgeCollectionName + ' \
                         Filter (e._from IN @nodeIds) \
                             Filter (e._to IN @nodeIds) \
-                            Filter @nodeDict[e._from]._community_SLPA != @nodeDict[e._to]._community_SLPA \
+                            Filter @nodeDict[e._from].@selectionAttribute != @nodeDict[e._to].@selectionAttribute \
                             RETURN e\
                     '
 
@@ -185,11 +254,16 @@ def CreateSimplifiedGraph(databaseName,originalGraphName,originalEdgeCollectionN
                     INTO '+ reducedEdgeCollectionName
     print('writing edges to new collection')
     cursor = db.aql.execute(query=query_str, bind_vars=bind_vars)
-    
+
+
+#CreateSimplifiedGraph(kg_db,originalGraphName,originalEdgeCollectionName,
+#                      originalNodeCollectionName, reducedGraphName, reducedEdgeCollectionName,reducedNodeCollectionName
+#                      )
+ 
 
 CreateSimplifiedGraph(miserablesDatabase,originalGraphName,originalEdgeCollectionName,
-                      originalNodeCollectionName, reducedGraphName, reducedEdgeCollectionName,reducedNodeCollectionName
-                      )
+                      originalNodeCollectionName, reducedGraphName, reducedEdgeCollectionName,reducedNodeCollectionName,
+                      method=method)
 
 #CreateSimplifiedGraph(eurovisDatabase,originalGraphName,originalEdgeCollectionName,
 #                      originalNodeCollectionName, reducedGraphName, reducedEdgeCollectionName,reducedNodeCollectionName,
